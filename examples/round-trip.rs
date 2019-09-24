204139326ffc47cc1431139765d5f075fd631c0c
Nick Fitzgerald <fitzgen@gmail.com>
2019-09-24 13:23:22 -0700

    WIP coz profiling

--- examples/round-trip.rs
+++ examples/round-trip.rs
@@ -14,13 +14,48 @@ fn main() {
 
 fn try_main() -> Result<(), failure::Error> {
     env_logger::init();
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(48)
+        .spawn_handler(|thread| {
+            std::thread::spawn(|| {
+                coz::thread_init();
+                thread.run()
+            });
+            Ok(())
+        })
+        .build_global()
+        .unwrap();
+
     let a = std::env::args().nth(1).ok_or_else(|| {
         failure::format_err!("must provide the input wasm file as the first argument")
     })?;
-    let m = walrus::Module::from_file(&a)?;
-    let wasm = m.emit_wasm();
-    if let Some(destination) = std::env::args().nth(2) {
-        std::fs::write(destination, wasm)?;
+    let n = std::env::args().nth(2).unwrap_or("100".into());
+    let n = n.parse::<usize>()?;
+
+    let buf = std::fs::read(a)?;
+
+    for _ in 0..n {
+        coz::begin!("round-trip");
+
+        // coz::begin!("parse");
+        let mut m = walrus::Module::from_buffer(&buf)?;
+        // coz::end!("parse");
+
+        // coz::begin!("gc");
+        walrus::passes::gc::run(&mut m);
+        // coz::end!("gc");
+
+        // coz::begin!("emit");
+        let wasm = m.emit_wasm();
+        // coz::end!("emit");
+
+        coz::end!("round-trip");
+
+        if let Some(destination) = std::env::args().nth(2) {
+            std::fs::write(destination, wasm)?;
+        }
     }
+
     Ok(())
 }
